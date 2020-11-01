ecf58ce9aa17e8ce08020c208c06b66adc2c6b6f
Eric Bridgeford <ebridge2@jhu.edu>
2020-11-01 15:50:02 -0500

    changed loc of diag removal

--- causal/causal_investigation_helpers.R
+++ causal/causal_investigation_helpers.R
@@ -2,8 +2,8 @@
 aal.homo.gr <- matrix(0, nrow=116, ncol=116)
 sdiag(aal.homo.gr, k=1) <- rep(c(1, 0), 116/2)[1:(116-1)]
 sdiag(aal.homo.gr, k=-1) <- rep(c(1, 0), 116/2)[1:(116-1)]
-aal.homo.gr <- as.vector(aal.homo.gr)
 diag(aal.homo.gr) <- NaN
+aal.homo.gr <- as.vector(aal.homo.gr)
 
 aal.hetero.gr <- matrix(0, nrow=116, ncol=116)
 for (i in 1:116) {
@@ -12,18 +12,19 @@ for (i in 1:116) {
   }
 }
 diag(aal.hetero.gr) <- NaN
+aal.hetero.gr <- as.vector(aal.hetero.gr)
 
 des.homo.gr <- matrix(0, nrow=70, ncol=70)
 sdiag(des.homo.gr, k = 35) <- 1
 sdiag(des.homo.gr, k=-35) <- 1
-des.homo.gr <- as.vector(des.homo.gr)
 diag(des.homo.gr) <- NaN
+des.homo.gr <- as.vector(des.homo.gr)
 
 des.hetero.gr <- matrix(0, nrow=70, ncol=70)
 des.hetero.gr[(70/2 + 1):70, (70/2 + 1):70] <- 1
 des.hetero.gr[1:(70/2), 1:(70/2)] <- 1
-des.hetero.gr <- as.vector(des.hetero.gr)
 diag(des.hetero.gr) <- NaN
+des.hetero.gr <- as.vector(des.hetero.gr)
 
 aal.comm <- list(Homotopic=aal.homo.gr, Homophilic=aal.hetero.gr)
 des.comm <- list(Homotopic=des.homo.gr, Homophilic=des.hetero.gr)
