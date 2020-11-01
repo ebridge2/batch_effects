b65658aba1a74aad718557b1f93b8a4f331422f8
Eric Bridgeford <ebridge2@jhu.edu>
2020-11-01 15:41:57 -0500

    updated causal investigation

--- causal/causal_driver_pdcorr.R
+++ causal/causal_driver_pdcorr.R
@@ -11,43 +11,16 @@ require(parallelDist)
 require(sva)
 require(mgcv)
 require(entropy)
+source('./causal_investigation_helpers.R')
 
 select <- dplyr::select
-fmri.path <- '/data/graphs/'
+in.path <- '/data/'
 n.vertices <- 116
-pheno.path <- '/data/phenotypic/CoRR_AggregatedPhenotypicData.csv'
+pheno.path <- file.path(in.path, 'phenotypic/CoRR_AggregatedPhenotypicData.csv')
 ncores <- detectCores() - 1
 parcellation <- "AAL"
-modality <- "fMRI"
-
-aal.homo.gr <- matrix(0, nrow=n.vertices, ncol=n.vertices)
-sdiag(aal.homo.gr, k=1) <- rep(c(1, 0), n.vertices/2)[1:(n.vertices-1)]
-sdiag(aal.homo.gr, k=-1) <- rep(c(1, 0), n.vertices/2)[1:(n.vertices-1)]
-aal.homo.gr <- as.vector(aal.homo.gr)
-aal.homo.gr <- aal.homo.gr[retain.dims]
-
-aal.hetero.gr <- matrix(0, nrow=n.vertices, ncol=n.vertices)
-for (i in 1:n.vertices) {
-  for (j in 1:n.vertices) {
-    aal.hetero.gr[i,j] <- ifelse((i %% 2) == (j %% 2), 1, 0)
-  }
-}
-
-des.homo.gr <- matrix(0, nrow=n.vertices, ncol=n.vertices)
-sdiag(des.homo.gr, k = 35) <- 1
-sdiag(des.homo.gr, k=-35) <- 1
-des.homo.gr <- as.vector(des.homo.gr)
-des.homo.gr <- des.homo.gr[retain.dims]
-
-des.hetero.gr <- matrix(0, nrow=n.vertices, ncol=n.vertices)
-des.hetero.gr[(n.vertices/2 + 1):n.vertices, (n.vertices/2 + 1):n.vertices] <- 1
-des.hetero.gr[1:(n.vertices/2), 1:(n.vertices/2)] <- 1
-des.hetero.gr <- as.vector(des.hetero.gr)
-des.hetero.gr <- des.hetero.gr[retain.dims]
-
-aal.comm <- list(Homotopic=aal.homo.gr, Homophilic=aal.hetero.gr)
-des.comm <- list(Homotopic=des.homo.gr, Homophilic=des.hetero.gr)
-parcel.comm <- list(AAL=aal.comm, Desikan=des.comm)
+modality <- "dMRI"
+mri.path <- file.path(in.path, modality, parcellation)
 
 
 cov.dat.dset <- cov.dat %>%
@@ -115,40 +88,7 @@ gr.dat <- gr.dat[retain.ids,]
 retain.dims <- sapply(1:dim(gr.dat)[2], function(j) var(gr.dat[,j])) > 0
 gr.dat <- gr.dat[,retain.dims]
 
-signal_ana <- function(data, cov.dat, parcellation="AAL") {
-  parcel.comm <- parcel.comm[[parcellation]]
-  do.call(rbind, lapply(names(parcel.comm), function(community) {
-    cmp.gr <- parcel.comm[[community]]
-    do.call(rbind, mclapply(1:dim(data)[1], function(i) {
-      dmeas <- data[i,]
-      signal <- median(dmeas[cmp.gr == 1]) - median(dmeas[cmp.gr == 0])
-      test.res <- wilcox.test(dmeas[cmp.gr == 1], dmeas[cmp.gr == 0], alternative = "greater")
-      return(data.frame(Dataset=cov.dat[i,"Dataset"], Subid=cov.dat[i,"Subid"], Session=cov.dat[i,"Session"],
-                        Signal=signal, Comparison=cmp, Effect.Size=test.res$statistic, p.value=test.res$p.value,
-                        Community=community, Parcellation=parcellation))
-    }, mc.cores = ncores))
-  }))
-}
-apply.along.dataset <- function(data, datasets, fn) {
-  for (dataset in unique(datasets)) {
-    this.dset <- data[datasets==dataset,]
-    data[datasets == dataset,] <- apply(this.dset, c(2), fn)
-  }
-  return(data)
-}
-
-zsc.batch <- function(x, datasets) {
-  return((x - mean(x))/sd(x))
-}
-
-ptr.batch <- function(x, datasets) {
-  nz <- which(x != 0)
-  x_rank = rank(x[nz])/(length(nz))
-  x[nz] <- x_rank
-  return(x)
-}
-
-results <- lapply(c("Raw", "Ranked", "Z-Score", "ComBat"), function(norm) {
+results.cont <- lapply(c("Raw", "Ranked", "Z-Score", "ComBat"), function(norm) {
   if (norm == "ComBat") {
     dat.norm <- t(ComBat(t(gr.dat), cov.dat$Dataset))
   } else if (norm == "Z-Score") {
@@ -161,10 +101,11 @@ results <- lapply(c("Raw", "Ranked", "Z-Score", "ComBat"), function(norm) {
   # exhaustively compute full distance matrix once since $$$
   Dmtx.norm <- as.matrix(parDist(dat.norm, threads=ncores))
 
-  result.site <- causal_ana_site(Dmtx.norm, cov.dat)
-  result.cov <- causal_ana_cov(Dmtx.norm, cov.dat)
-  result.cov.cont <- causal_ana_cov_cont(Dmtx.norm, cov.dat)
-  result.signal <- signal_ana(dat.norm, cov.dat, parcellation=parcellation)
+  result.site <- causal_ana_site(Dmtx.norm, cov.dat, mc.cores=ncores)
+  result.cov <- causal_ana_cov(Dmtx.norm, cov.dat, mc.cores=ncores)
+  result.cov.cont <- causal_ana_cov_cont(Dmtx.norm, cov.dat, mc.cores=ncores)
+  result.signal <- signal_ana(dat.norm, cov.dat, parcellation=parcellation, mc.cores=ncores,
+                              retain.dims=retain.dims)
   return(list(Site=result.site %>% mutate(Method=norm),
               Covariate=result.cov %>% mutate(Method=norm),
               Covariate.Cont=result.cov.cont %>% mutate(Method=norm),
@@ -175,10 +116,11 @@ result.site <- do.call(rbind, lapply(results, function(res) res$Site)) %>%
   mutate(Modality=modality)
 result.cov <- do.call(rbind, lapply(results, function(res) res$Covariate)) %>%
   mutate(Modality=modality)
-result.cov.cont <- do.call(rbind, lapply(results, function(res) res$Covariate.Cont)) %>%
-  mutate(Modality=modality)
+# result.cov.cont <- do.call(rbind, lapply(results, function(res) res$Covariate.Cont)) %>%
+#   mutate(Modality=modality)
 result.signal <- do.call(rbind, lapply(results, function(res) res$Signal)) %>%
   mutate(Modality=modality)
 
-saveRDS(list(Site=result.site, Covariate=result.cov, Covariate.Cont=result.cov.cont, Signal=result.signal),
-        file=sprintf('/base/data/pdcorr_outputs_%s_%s.rds', modality, parcellation))
+saveRDS(list(Site=result.site, Covariate=result.cov, #Covariate.Cont=result.cov.cont,
+             Signal=result.signal),
+        file=sprintf('/base/data/dcorr/pdcorr_outputs_%s_%s.rds', modality, parcellation))
--- causal/causal_investigation_helpers.R
+++ causal/causal_investigation_helpers.R
@@ -1,4 +1,33 @@
 
+aal.homo.gr <- matrix(0, nrow=116, ncol=116)
+sdiag(aal.homo.gr, k=1) <- rep(c(1, 0), 116/2)[1:(116-1)]
+sdiag(aal.homo.gr, k=-1) <- rep(c(1, 0), 116/2)[1:(116-1)]
+aal.homo.gr <- as.vector(aal.homo.gr)
+aal.homo.gr <- aal.homo.gr[retain.dims]
+
+aal.hetero.gr <- matrix(0, nrow=116, ncol=116)
+for (i in 1:116) {
+  for (j in 1:116) {
+    aal.hetero.gr[i,j] <- ifelse((i %% 2) == (j %% 2), 1, 0)
+  }
+}
+
+des.homo.gr <- matrix(0, nrow=70, ncol=70)
+sdiag(des.homo.gr, k = 35) <- 1
+sdiag(des.homo.gr, k=-35) <- 1
+des.homo.gr <- as.vector(des.homo.gr)
+des.homo.gr <- des.homo.gr[retain.dims]
+
+des.hetero.gr <- matrix(0, nrow=70, ncol=70)
+des.hetero.gr[(70/2 + 1):70, (70/2 + 1):70] <- 1
+des.hetero.gr[1:(70/2), 1:(70/2)] <- 1
+des.hetero.gr <- as.vector(des.hetero.gr)
+des.hetero.gr <- des.hetero.gr[retain.dims]
+
+aal.comm <- list(Homotopic=aal.homo.gr, Homophilic=aal.hetero.gr)
+des.comm <- list(Homotopic=des.homo.gr, Homophilic=des.hetero.gr)
+parcel.comm <- list(AAL=aal.comm, Desikan=des.comm)
+
 compute_propensities <- function(df, form="Treatment ~ Sex + Age + Continent", trim=.01) {
   df$prop_scores <- glm(form, family=binomial(link="logit"), data=df)$fitted.values
   df <- df %>%
@@ -10,28 +39,29 @@ compute_propensities <- function(df, form="Treatment ~ Sex + Age + Continent", t
   return(df)
 }
 
-causal_ana_site <- function(Dmtx.dat, cov.dat, trim=.01, R=500) {
+causal_ana_site <- function(Dmtx.dat, cov.dat, trim=.01, R=500, mc.cores=1) {
   datasets <- sort(unique((cov.dat %>% dplyr::select(Dataset))$Dataset))
   dset.pairs <- combn(datasets, 2)
   result <- do.call(rbind, mclapply(1:dim(dset.pairs)[2], function(x) {
+    result <- list()
+    # get first and second dset
+    dset.1 <- dset.pairs[1,x]
+    dset.2 <- dset.pairs[2,x]
+    n.1 <- sum(cov.dat$Dataset == dset.1)
+    n.2 <- sum(cov.dat$Dataset == dset.2)
+    # Control is always larger of the two for all combn
+    if (n.1 < n.2) {
+      dset.i <- dset.1; dset.j <- dset.2
+      n.i <- n.1; n.j <- n.2
+    } else if (n.1 >= n.2) {
+      dset.i <- dset.2; dset.j <- dset.1
+      n.i <- n.2; n.j <- n.1
+    }
+    cov.dat.ij <- cov.dat %>%
+      filter(Dataset %in% c(dset.i, dset.j)) %>%
+      mutate(Treatment = as.numeric(Dataset == dset.i))
+    ov.ij=compute_overlap(cov.dat.ij %>% filter(Dataset == dset.i), cov.dat.ij %>% filter(Dataset == dset.j))
     tryCatch({
-      result <- list()
-      # get first and second dset
-      dset.1 <- dset.pairs[1,x]
-      dset.2 <- dset.pairs[2,x]
-      n.1 <- sum(cov.dat$Dataset == dset.1)
-      n.2 <- sum(cov.dat$Dataset == dset.2)
-      # Control is always larger of the two for all combn
-      if (n.1 < n.2) {
-        dset.i <- dset.1; dset.j <- dset.2
-        n.i <- n.1; n.j <- n.2
-      } else if (n.1 >= n.2) {
-        dset.i <- dset.2; dset.j <- dset.1
-        n.i <- n.2; n.j <- n.1
-      }
-      cov.dat.ij <- cov.dat %>%
-        filter(Dataset %in% c(dset.i, dset.j)) %>%
-        mutate(Treatment = as.numeric(Dataset == dset.i))
       Dmtx.dat.ij <- Dmtx.dat[cov.dat.ij$id, cov.dat.ij$id]
       ## Uncorrected
       test.uncor <- pdcor.test(as.dist(Dmtx.dat.ij), cov.dat.ij$Treatment,
@@ -39,8 +69,8 @@ causal_ana_site <- function(Dmtx.dat, cov.dat, trim=.01, R=500) {
                                  mutate(Continent=as.numeric(Continent), Sex=as.numeric(Sex),
                                         Age=as.numeric(Age)), R=R)
       result$uncor <- data.frame(Data="Untrimmed", Method="PDcor", Dataset.Trt=dset.i,
-                                 Dataset.Ctrl=dset.j, Effect=test.uncor$estimate,
-                                 PValue=test.uncor$p.value)
+                                 Dataset.Ctrl=dset.j, Effect.Name="Site", Effect=test.uncor$estimate,
+                                 p.value=test.uncor$p.value, Overlap=ov.ij)
 
       ## Trimmed
       if (length(unique((cov.dat.ij %>% filter(Dataset == dset.i) %>% dplyr::select(Sex))$Sex)) == 1) {
@@ -60,69 +90,87 @@ causal_ana_site <- function(Dmtx.dat, cov.dat, trim=.01, R=500) {
           test.trim <- pdcor.test(as.dist(Dmtx.dat.ij.trim), cov.dat.ij.trim$Treatment,
                                   as.matrix(cov.dat.ij.trim %>% dplyr::select(Continent, Sex, Age)), R=R)
           result$trim <- data.frame(Data="Trimmed", Method="PDcor", Dataset.Trt=dset.i,
-                                    Dataset.Ctrl=dset.j, Effect=test.trim$estimate,
-                                    p.value=test.trim$p.value, Overlap=compute_overlap(cov.dat.ij %>% filter(Dataset == dset.i),
-                                                                                       cov.dat.ij %>% filter(Dataset == dset.j)))
+                                    Dataset.Ctrl=dset.j, Effect.Name="Site", Effect=test.trim$estimate,
+                                    p.value=test.trim$p.value, Overlap=ov.ij)
         }
       }
       return(do.call(rbind, result) %>%
                mutate(Effect.Name="Site"))
     }, error=function(e) {
       return(data.frame(Data=c("Untrimmed", "Trimmed"), Method="PDcor", Dataset.Trt=dset.i,
-                        Dataset.Ctrl=dset.j, Effect.Name="Site", Effect=NA, PValue=NA))
+                        Dataset.Ctrl=dset.j, Effect.Name="Site", Effect=NA, p.value=NA, Overlap=compute_overlap(cov.dat.ij %>% filter(Dataset == dset.i),
+                                                                                                               cov.dat.ij %>% filter(Dataset == dset.j))))
     })
-  }, mc.cores = ncores))
+  }, mc.cores = mc.cores))
   return(result)
 }
 
-compute_effect <- function(D.i, cov.dat.i, E1.name, E2.name, R=1000, nboots=100, ...) {
-  n.i <- length(cov.dat.i$id)
+compute_effect <- function(D.i, cov.dat.i, E1.name, E2.name, R=1000, nboots=100, mc.cores=1, ...) {
+  if (length(unique(cov.dat.i$Dataset)) == 1) {
+    col.name <- "Dataset"
+    col.id <- unique(as.character(cov.dat.i$Dataset))
+  } else if (length(unique(cov.dat.i$Continent)) == 1) {
+    col.name <- "Continent"
+    col.id <- unique(as.character(cov.dat.i$Continent))
+  } else {
+    return(NULL)
+  }
+  tryCatch({
+    n.i <- length(cov.dat.i$id)
 
-  test.uncor <- pdcor.test(D.i, as.numeric(cov.dat.i[[E1.name]]),
-                           as.numeric(cov.dat.i[[E2.name]]), R=R)
-  ## Bootstrapped CIs
-  bootstr.ci <- sapply(1:nboots, function(i) {
-    ids <- sample(1:n.i, size=n.i, replace = TRUE)
-    cov.dat.boot <- cov.dat.i[ids,]
-    Dmtx.dat.boot <- as.dist(as.matrix(D.i)[ids, ids])
-    test.boot <- pdcor(Dmtx.dat.boot, as.numeric(cov.dat.boot[[E1.name]]),
-                       as.numeric(cov.dat.boot[[E2.name]]))
-    return(test.boot)
+    test.uncor <- pdcor.test(D.i, as.numeric(cov.dat.i[[E1.name]]),
+                             as.numeric(cov.dat.i[[E2.name]]), R=R)
+    ## Bootstrapped CIs
+    bootstr.ci <- simplify2array(mclapply(1:nboots, function(i) {
+      ids <- sample(1:n.i, size=n.i, replace = TRUE)
+      cov.dat.boot <- cov.dat.i[ids,]
+      Dmtx.dat.boot <- as.dist(as.matrix(D.i)[ids, ids])
+      test.boot <- pdcor(Dmtx.dat.boot, as.numeric(cov.dat.boot[[E1.name]]),
+                         as.numeric(cov.dat.boot[[E2.name]]))
+      return(test.boot)
+    }, mc.cores=mc.cores))
+    ci.npboot <- quantile(bootstr.ci, c(.025, .975))
+
+    jk.stat <- simplify2array(mclapply(1:n.i, function(i) {
+      ids <- (1:n.i)[-i]
+      cov.dat.boot <- cov.dat.i[ids,]
+      Dmtx.dat.boot <- as.dist(as.matrix(D.i)[ids, ids])
+      test.boot <- pdcor(Dmtx.dat.boot, as.numeric(cov.dat.boot[[E1.name]]),
+                         as.numeric(cov.dat.boot[[E2.name]]))
+      return(test.boot)
+    }))
+    psi <- test.uncor$estimate*n.i - (n.i - 1)*jk.stat
+    ps.mean <- mean(psi); ps.var <- var(psi)
+    ci.jk <- c(ps.mean + qnorm(.025)*sqrt(1/n.i*ps.var),
+               ps.mean + qnorm(.975)*sqrt(1/n.i*ps.var))
+
+    result <- data.frame(Method="PDcor", Effect.Name=E1.name, Effect=test.uncor$estimate, Effect.lwr.jk=ci.jk[1], Effect.upr.jk=ci.jk[2],
+                      Effect.lwr.npboot=ci.npboot[1], Effect.upr.npboot=ci.npboot[2],
+                      p.value=test.uncor$p.value, Entropy=entropy(as.numeric(cov.dat.i[[E1.name]]), method="MM"), Variance=var(as.numeric(cov.dat.i[[E1.name]])),
+                      n=n.i, N=length(unique(cov.dat.i$Subid)))
+  }, error=function(e) {
+    result <- data.frame(Method="PDcor", Effect.Name=E1.name, Effect=NA, Effect.lwr.jk=NA, Effect.upr.jk=NA,
+                      Effect.lwr.npboot=NA, Effect.upr.npboot=NA, Effect.lwr.adjboot=NA, Effect.upr.adjboot=NA,
+                      p.value=NA, Entropy=NA, Variance=NA,
+                      n=n.i, N=length(unique(cov.dat.i$Subid)))
   })
-  ci.npboot <- quantile(bootstr.ci, c(.025, .975))
-
-  jk.stat <- sapply(1:n.i, function(i) {
-    ids <- (1:n.i)[-i]
-    cov.dat.boot <- cov.dat.i[ids,]
-    Dmtx.dat.boot <- as.dist(as.matrix(D.i)[ids, ids])
-    test.boot <- pdcor(Dmtx.dat.boot, as.numeric(cov.dat.boot[[E1.name]]),
-                       as.numeric(cov.dat.boot[[E2.name]]))
-    return(test.boot)
-  })
-  psi <- test.uncor$estimate*n.i - (n.i - 1)*jk.stat
-  ps.mean <- mean(psi); ps.var <- var(psi)
-  ci.jk <- c(ps.mean + qnorm(.025)*sqrt(1/n.i*ps.var),
-             ps.mean + qnorm(.975)*sqrt(1/n.i*ps.var))
-
-  return(data.frame(Method="PDcor", Effect.Name=E1.name, Continent=continent, Effect=res$estimate, Effect.lwr.jk=ci.jk[1], Effect.upr.jk=ci.jk[2],
-                    Effect.lwr.npboot=ci.npboot[1], Effect.upr.npboot=ci.npboot[2], Effect.lwr.adjboot=ci.adjboot[1], Effect.upr.adjboot=ci.adjboot[2],
-                    PValue=test.uncor$p.value, Entropy=entropy(as.numeric(cov.dat.i[[E1.name]]), method="MM"), Variance=var(as.numeric(cov.dat.i[[E1.name]])),
-                    n=n.i, N=length(unique(cov.dat.i$Subid))))
+  result[[col.name]] <- col.id
+  return(result)
 }
 
-causal_ana_cov <- function(Dmtx.dat, cov.dat, nboots=100, R=1000) {
+causal_ana_cov <- function(Dmtx.dat, cov.dat, nboots=100, R=1000, mc.cores=1) {
   cov.dat <- cov.dat %>%
     ungroup() %>%
     mutate(id=row_number())
   datasets <- sort(unique((cov.dat %>% dplyr::select(Dataset))$Dataset))
-  result.sex <- do.call(rbind, mclapply(datasets, function(dataset) {
+  result.sex <- do.call(rbind, lapply(datasets, function(dataset) {
     tryCatch({
       cov.dat.dset <- cov.dat %>%
         filter(Dataset == dataset)
 
       n.i <- length(cov.dat.dset$id)
       Dmtx.dat.dset <- as.dist(Dmtx.dat[cov.dat.dset$id, cov.dat.dset$id])
-      res <- compute_effect(Dmtx.dat.dset, cov.dat.dset, "Sex", "Age", R=R, nboots=nboots)
+      res <- compute_effect(Dmtx.dat.dset, cov.dat.dset, "Sex", "Age", R=R, nboots=nboots, mc.cores=mc.cores)
 
       return(res)
     }, error=function(e) {
@@ -130,59 +178,61 @@ causal_ana_cov <- function(Dmtx.dat, cov.dat, nboots=100, R=1000) {
       return(NULL)
     })
 
-  }, mc.cores=ncores))
+  }))
 
-  result.age <- do.call(rbind, mclapply(datasets, function(dataset) {
+  result.age <- do.call(rbind, lapply(datasets, function(dataset) {
     tryCatch({
       cov.dat.dset <- cov.dat %>%
         filter(Dataset == dataset)
 
       n.i <- length(cov.dat.dset$id)
       Dmtx.dat.dset <- as.dist(Dmtx.dat[cov.dat.dset$id, cov.dat.dset$id])
-      res <- compute_effect(Dmtx.dat.dset, cov.dat.dset, "Age", "Sex", R=R, nboots=nboots)
+      res <- compute_effect(Dmtx.dat.dset, cov.dat.dset, "Age", "Sex", R=R, nboots=nboots, mc.cores=mc.cores)
 
       return(res)
     }, error=function(e) {
       return(NULL)
     })
-  }, mc.cores=ncores))
+  }))
   return(rbind(result.sex, result.age))
 }
 
-causal_ana_cov_cont <- function(Dmtx.dat, cov.dat, nboots=100, R=1000) {
+causal_ana_cov_cont <- function(Dmtx.dat, cov.dat, nboots=100, R=1000, mc.cores=1) {
   cov.dat <- cov.dat %>%
     ungroup() %>%
     mutate(id=row_number())
   continents <- sort(unique((cov.dat %>% dplyr::select(Continent))$Continent))
-  result.sex <- do.call(rbind, mclapply(continents, function(continent) {
+  result.sex <- do.call(rbind, lapply(continents, function(continent) {
     tryCatch({
       cov.dat.dset <- cov.dat %>%
         filter(Continent == continent)
 
       n.i <- length(cov.dat.dset$id)
       Dmtx.dat.dset <- as.dist(Dmtx.dat[cov.dat.dset$id, cov.dat.dset$id])
-      res <- compute_effect(Dmtx.dat.dset, cov.dat.dset, "Sex", "Age", R=R, nboots=nboots)
+      res <- compute_effect(Dmtx.dat.dset, cov.dat.dset, "Sex", "Age", R=R, nboots=nboots,
+                            mc.cores=mc.cores)
 
       return(res)
     }, error=function(e) {
       return(NULL)
     })
-  }, mc.cores=ncores))
+  }))
 
-  result.age <- do.call(rbind, mclapply(continents, function(continent) {
+  result.age <- do.call(rbind, lapply(continents, function(continent) {
     tryCatch({
       cov.dat.dset <- cov.dat %>%
         filter(Continent == continent)
 
       n.i <- length(cov.dat.dset$id)
       Dmtx.dat.dset <- as.dist(Dmtx.dat[cov.dat.dset$id, cov.dat.dset$id])
-      res <- compute_effect(Dmtx.dat.dset, cov.dat.dset, "Age", "Sex", R=R, nboots=nboots)
+      res <- compute_effect(Dmtx.dat.dset, cov.dat.dset, "Age", "Sex", R=R, nboots=nboots,
+                            mc.cores=mc.cores)
 
       return(res)
     }, error=function(e) {
       return(NULL)
     })
-  }, mc.cores=ncores))
+  }))
   return(rbind(result.sex, result.age))
 }
 
@@ -234,20 +284,24 @@ compute_overlap <- function(X1, X2) {
   return(as.numeric(unique((X1$Continent)) == unique(X2$Continent))*per.ov)
 }
 
-signal_ana <- function(data, cov.dat, parcellation="AAL") {
-  parcel.comm <- parcel.comm[[parcellation]]
-  do.call(rbind, lapply(names(parcel.comm), function(community) {
-    cmp.gr <- parcel.comm[[community]]
+signal_ana <- function(data, cov.dat, parcellation="AAL", mc.cores=1, retain.dims=NULL) {
+  if (is.null(retain.ids)) {
+    stop("Pass removal indices.")
+  }
+  parc.c <- parcel.comm[[parcellation]]
+  do.call(rbind, lapply(names(parc.c), function(community) {
+    cmp.gr <- parc.c[[community]][retain.dims]
     do.call(rbind, mclapply(1:dim(data)[1], function(i) {
       dmeas <- data[i,]
       signal <- median(dmeas[cmp.gr == 1]) - median(dmeas[cmp.gr == 0])
       test.res <- wilcox.test(dmeas[cmp.gr == 1], dmeas[cmp.gr == 0], alternative = "greater")
       return(data.frame(Dataset=cov.dat[i,"Dataset"], Subid=cov.dat[i,"Subid"], Session=cov.dat[i,"Session"],
-                        Signal=signal, Comparison=cmp, Effect.Size=test.res$statistic, p.value=test.res$p.value,
+                        Signal=signal, Effect.Size=test.res$statistic, p.value=test.res$p.value,
                         Community=community, Parcellation=parcellation))
-    }, mc.cores = ncores))
+    }, mc.cores = mc.cores))
   }))
 }
+
 apply.along.dataset <- function(data, datasets, fn) {
   for (dataset in unique(datasets)) {
     this.dset <- data[datasets==dataset,]
